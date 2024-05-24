acb72847ad2c597db126c0f8ab50fc7d83930a9b
Colby M. White <colbywhite@users.noreply.github.com>
2024-05-23 20:05:04 -0500

    feat: add htmx
        yarn add astro-htmx htmx.org

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import htmx from 'astro-htmx';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), htmx()]
 });
