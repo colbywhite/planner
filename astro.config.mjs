bbe3c435c21ef03032e8cbe89ca414ed9746d2ac
Colby M. White <colbywhite@users.noreply.github.com>
2024-05-23 20:11:48 -0500

    feat: add alpinejs
        yarn astro add alpinejs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 import htmx from 'astro-htmx';
+import alpinejs from "@astrojs/alpinejs";
 
 export default defineConfig({
-  integrations: [tailwind(), htmx()]
+  integrations: [tailwind(), htmx(), alpinejs()]
 });
