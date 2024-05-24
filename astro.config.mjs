89e61bf795282fa50d54aa8bc18d5d696df1b7ba
Colby M. White <colbywhite@users.noreply.github.com>
2024-05-23 22:20:17 -0500

    feat: add ssr
        yarn astro add node

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import htmx from 'astro-htmx';
 import alpinejs from "@astrojs/alpinejs";
+import node from "@astrojs/node";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), htmx(), alpinejs()]
+  integrations: [tailwind(), htmx(), alpinejs()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
