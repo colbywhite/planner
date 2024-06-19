de01dfd061a45a1429214a890b80b74d72e47e4c
Colby M. White <colbywhite@users.noreply.github.com>
2024-06-19 18:54:03 -0500

    feat: list, create blocks

--- src/services/block.service.ts
+++ src/services/block.service.ts
@@ -11,17 +11,31 @@ export class BlockService {
         this.client = createSupabaseClient(cookies)
     }
 
+    public async countBlocks() {
+        const {count, error} = await this.client.from('Block')
+            .select('*', {count: 'exact', head: true})
+        if (error) {
+            throw error
+        }
+        return count
+    }
+
     public async getBlock(index: number) {
+        const lastBlockIndex = (await this.countBlocks()) - 1;
+        const safeIndex = Math.max(0, Math.min(index, lastBlockIndex))
+        const previousIndex = safeIndex - 1 >= 0 ? safeIndex - 1 : undefined
+        const nextIndex = safeIndex + 1 <= lastBlockIndex ? safeIndex + 1 : undefined
+
         const {data, error} = await this.client.from('Block')
             .select()
             .order('start', {ascending: true})
-            .range(index, index + 1)
+            .range(safeIndex, safeIndex + 1)
             .limit(1)
             .single()
         if (error) {
             throw error
         }
-        return data
+        return {block: data, index: safeIndex, previousIndex, nextIndex}
     }
 
     public async createBlock(block: BlockInsert) {
