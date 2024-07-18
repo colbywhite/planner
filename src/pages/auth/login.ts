197e0bde164b11c8fc88b0db1478c1dcb593af8d
Colby M. White <colbywhite@users.noreply.github.com>
2024-07-18 17:59:10 -0500

    feat: prefetch prev/next links

--- src/pages/auth/login.ts
+++ src/pages/auth/login.ts
@@ -3,6 +3,7 @@ import {signInWithGithub} from "../../services/auth.service";
 import {createSupabaseClient} from "../../services/supabase.service";
 
 export const GET: APIRoute = async ({request, cookies, redirect}) => {
+    console.log('/auth/login', request.referrer, request.headers)
     const client = createSupabaseClient(cookies)
     const redirectUrl = await signInWithGithub(client, request.url)
     return redirect(redirectUrl || '/')
