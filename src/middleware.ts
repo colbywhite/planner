391c4ca331c3176df45d812f1fc7982461a52852
Colby M. White <colbywhite@users.noreply.github.com>
2024-07-20 16:38:00 -0500

    feat: htmx-driven paging; loading indicators

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,13 +5,11 @@ const LOGIN_ROUTE = '/login' as const
 const AUTH_PARENT_ROUTE = '/auth' as const
 
 export const onRequest = defineMiddleware(async ({url: {pathname}, redirect, cookies, locals}, next) => {
-    console.log('middleware', {pathname, locals})
     if (pathname === LOGIN_ROUTE || pathname.startsWith(AUTH_PARENT_ROUTE)) {
         return next()
     }
     const client = createSupabaseClient(cookies)
     const {data: {user}} = await client.auth.getUser()
-    console.log('middleware', {pathname, user, locals})
     if (user === null) {
         return redirect(LOGIN_ROUTE)
     }
